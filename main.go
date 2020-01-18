6c5c9206f3e34cfa031a9c6420ef52173006ca42
Jason Mooberry <jason.mooberry@gmail.com>
2020-01-18 18:29:58 -0500

    first

---
+++ main.go
@@ -0,0 +1,210 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+type (
+	Matrix struct {
+		rs   [][]rune
+		xmax int
+		ymax int
+
+		seen [][]bool
+
+		walks int
+	}
+
+	Rune struct {
+		x, y int
+		c    rune
+	}
+
+	RuneSet []Rune
+)
+
+func (rs RuneSet) String() string {
+	var (
+		ps    [][2]int
+		runes []rune
+	)
+	for _, r := range rs {
+		ps = append(ps, [2]int{r.x, r.y})
+		runes = append(runes, r.c)
+	}
+	return fmt.Sprintf("%s: %v", string(runes), ps)
+}
+
+func (rs *RuneSet) Push(x, y int, c rune) {
+	*rs = append(*rs, Rune{x: x, y: y, c: c})
+}
+
+func (rs *RuneSet) Pop() {
+	*rs = (*rs)[:len(*rs)-1]
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func NewMatrix(x, y int) *Matrix {
+	m := &Matrix{
+		rs:   make([][]rune, y),
+		seen: make([][]bool, y),
+		xmax: x,
+		ymax: y,
+	}
+	for y, _ := range m.rs {
+		m.rs[y] = make([]rune, x)
+		m.seen[y] = make([]bool, x)
+	}
+	return m
+}
+
+func (m *Matrix) Randomize(chars string) {
+	rs := []rune(chars)
+	for y := 0; y < m.ymax; y++ {
+		for x := 0; x < m.xmax; x++ {
+			m.rs[y][x] = rs[rand.Intn(len(rs))]
+		}
+	}
+}
+
+func (m *Matrix) FindWords(trie Trie) []string {
+
+	var (
+		node *TrieNode
+
+		words []string
+		runes = make(RuneSet, 0)
+
+		walk func(x, y int)
+	)
+
+	walk = func(x, y int) {
+
+		// just for stats
+		m.walks++
+
+		// skip this path if we hit a seen block
+		if m.seen[y][x] {
+			return
+		}
+		m.seen[y][x] = true
+		defer func() { m.seen[y][x] = false }()
+
+		c := m.rs[y][x]
+
+		if node == nil {
+			n, exists := trie[c]
+			if !exists {
+				fmt.Println(string(c), "not found in root of trie?")
+				return
+			}
+			node = n
+		} else {
+			n, exists := node.next[c]
+			if !exists {
+				return
+			}
+			prev := node
+			defer func() { node = prev }()
+			node = n
+		}
+
+		runes.Push(x, y, c)
+		defer runes.Pop()
+
+		if node.eow {
+			words = append(words, runes.String())
+		}
+
+		// if row above
+		if y > 0 {
+			if x > 0 {
+				walk(x-1, y-1)
+			}
+			walk(x, y-1)
+			if x < m.xmax-1 {
+				walk(x+1, y-1)
+			}
+		}
+		// current row on either side
+		if x > 0 {
+			walk(x-1, y)
+		}
+		if x < m.xmax-1 {
+			walk(x+1, y)
+		}
+		// if row below
+		if y < m.ymax-1 {
+			if x > 0 {
+				walk(x-1, y+1)
+			}
+			walk(x, y+1)
+			if x < m.xmax-1 {
+				walk(x+1, y+1)
+			}
+		}
+
+	}
+
+	for y := 0; y < m.ymax; y++ {
+		for x := 0; x < m.xmax; x++ {
+			walk(x, y)
+			node = nil
+		}
+	}
+
+	return words
+
+}
+
+func (m *Matrix) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('+')
+	fmt.Fprint(&buf, strings.Repeat("-", m.xmax))
+	buf.WriteByte('+')
+	buf.WriteByte('\n')
+	for y := 0; y < m.ymax; y++ {
+		buf.WriteByte('|')
+		for x := 0; x < m.xmax; x++ {
+			buf.WriteRune(m.rs[y][x])
+		}
+		buf.WriteByte('|')
+		buf.WriteByte('\n')
+	}
+	buf.WriteByte('+')
+	fmt.Fprint(&buf, strings.Repeat("-", m.xmax))
+	buf.WriteByte('+')
+	buf.WriteByte('\n')
+	return buf.String()
+}
+
+const (
+	fullAlphabet = `abcdefghijklmnopqrstuvwxyz`
+)
+
+func main() {
+
+	start := time.Now()
+	trie := LoadTrie("/usr/share/dict/words")
+	fmt.Println("loaded trie in", time.Since(start))
+
+	m := NewMatrix(128, 128)
+	m.Randomize(fullAlphabet + fullAlphabet)
+	fmt.Println(m)
+
+	start = time.Now()
+	words := m.FindWords(trie)
+	fmt.Println(m.walks, "walks found", len(words), "words in", time.Since(start))
+
+	for _, word := range words {
+		fmt.Println(word)
+	}
+
+}
---
+++ trie.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+)
+
+type (
+	Trie map[rune]*TrieNode
+
+	TrieNode struct {
+		char rune
+		eow  bool
+		next map[rune]*TrieNode
+	}
+)
+
+func NewTrieNode(c rune) *TrieNode {
+	return &TrieNode{
+		char: c,
+		next: make(map[rune]*TrieNode),
+	}
+}
+
+func (tn *TrieNode) String() string {
+	var cs []rune
+	for c, _ := range tn.next {
+		cs = append(cs, c)
+	}
+	sort.Slice(cs, func(i, j int) bool {
+		return cs[i] < cs[j]
+	})
+	return fmt.Sprintf("%q: %q", tn.char, cs)
+}
+
+func (t Trie) AddWord(word string) {
+
+	// drop single char words
+	if len(word) < 2 {
+		return
+	}
+
+	word = strings.ToLower(word)
+
+	var node *TrieNode
+
+	for _, c := range word {
+		if node == nil {
+			var exists bool
+			node, exists = t[c]
+			if !exists {
+				node = NewTrieNode(c)
+				t[c] = node
+			}
+			continue
+		}
+
+		next, exists := node.next[c]
+		if !exists {
+			next = NewTrieNode(c)
+			node.next[c] = next
+		}
+
+		node = next
+	}
+
+	node.eow = true
+
+}
+
+func LoadTrie(rootPath string) map[rune]*TrieNode {
+
+	trie := make(Trie)
+
+	file, err := os.OpenFile(rootPath, os.O_RDONLY, 0)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	sc := bufio.NewScanner(file)
+
+	for sc.Scan() {
+		trie.AddWord(sc.Text())
+	}
+
+	return trie
+
+}
